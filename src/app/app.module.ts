711c818a8e813dce3411cc25df820b3fb83a0557
AlmironLeandro <leandroealmiron@hotmail.com>
2023-03-08 10:59:32 -0300

    push intro, about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroComponent } from './Pages/intro/intro.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './Pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    NavbarComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
