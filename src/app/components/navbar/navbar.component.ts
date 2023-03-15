6439486c2eb554e2ae3f29757cf068b6ece49c9f
AlmironLeandro <leandroealmiron@hotmail.com>
2023-03-15 07:14:07 -0300

    navbar responsive

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  @ViewChild('menu') item!: ElementRef;
 
+  openMenu() {
+    const menuChildren = this.item.nativeElement.childNodes;
+    const menu = this.item.nativeElement
+    menuChildren.forEach((child: any, i: any) => {
+      if (i !== 0) {
+        child.classList.toggle('open__navbar')
+      }
+    },
+    menu.classList.toggle('menuVisible'));
+  }
 }
